7cf14f580a0f2f88ec40629537cc6ade98b90ce7
floatingbug <thomas.hof1984@gmail.com>
2025-08-11 11:21:05 +0200

    fix: Weiterleitung nach Buchung korrigieren

--- client/src/modules/bookSpace/BookSpaceView.vue
+++ client/src/modules/bookSpace/BookSpaceView.vue
@@ -1,6 +1,6 @@
 <script setup>
-import {ref, reactive, onMounted} from "vue";
-import {useRoute, useRouter} from "vue-router";
+import { ref, reactive, onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import fetchSpaceById from "./api/fetchSpaceById.js";
 import SpaceInformations from "./components/SpaceInformations.vue";
 import BookSpaceForm from "./components/BookSpaceForm.vue";
@@ -9,61 +9,57 @@ import fetchBookingsAPI from "./api/fetchBookingsAPI.js";
 import { useToast } from "primevue/usetoast";
 const toast = useToast();
 
-
 const route = useRoute();
 const router = useRouter();
 const space = ref();
 const bookings = ref();
 
-
 onMounted(async () => {
-	const fetchedSpace = await fetchSpaceById({spaceId: route.query.spaceId});
-	space.value = fetchedSpace.data;
+    const fetchedSpace = await fetchSpaceById({ spaceId: route.query.spaceId });
+    space.value = fetchedSpace.data;
 
-	const fetchedBookings = await fetchBookingsAPI({spaceId: route.query.spaceId});
-	bookings.value = fetchedBookings.data;
+    const fetchedBookings = await fetchBookingsAPI({ spaceId: route.query.spaceId });
+    bookings.value = fetchedBookings.data;
 });
 
+async function onBookSpaceFormAction(event) {
+    const result = await bookSpaceAPI({ booking: event.booking });
 
-async function onBookSpaceFormAction(event){
-	const result = await bookSpaceAPI({booking: event.booking});
+    if (!result.success) {
+        toast.add({ severity: "warn", summary: "Booking failed", detail: result.errors[0], life: 5000 });
+    }
 
-	if(!result.success) {
-		toast.add({ severity: 'warn', summary: 'Booking failed', detail: result.errors[0], life: 5000 });
-	}
-
-	router.push("/my-bookings");
+    router.push("/dashboard");
 }
-
 </script>
 
-
 <template>
-	<div v-if="space && bookings" class="layout">
-		<div class="space-booking">
-			<SpaceInformations class="space-informations" :space="space" />
+    <div v-if="space && bookings" class="layout">
+        <div class="space-booking">
+            <SpaceInformations class="space-informations" :space="space" />
 
-			<Divider></Divider>
-			
-			<BookSpaceForm class="book-space-form" 
-				:space="space"
-				:bookings="bookings"
-				@bookSpaceForm:action="onBookSpaceFormAction" 
-			/>
-		</div>
-	</div>
+            <Divider></Divider>
+
+            <BookSpaceForm
+                class="book-space-form"
+                :space="space"
+                :bookings="bookings"
+                @bookSpaceForm:action="onBookSpaceFormAction"
+            />
+        </div>
+    </div>
 </template>
-
 
 <style scoped>
 .space-booking {
-	display: flex;
-	flex-direction: column;
-	gap: 2rem;
-	padding: 0 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    padding: 0 1rem;
 }
 
-.space-informations, .book-space-form {
-	flex: 1;
+.space-informations,
+.book-space-form {
+    flex: 1;
 }
 </style>
